64d9071ccb5b791e6708412434c09d58d5cc3bab
DJ Mungin <103383531+OZ4Tech@users.noreply.github.com>
2022-08-25 21:47:18 -0400

    Add files via upload

---
+++ tests/Employee.test.js
@@ -0,0 +1,5 @@
+const Employee = require('./Employee');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
---
+++ tests/Engineer.test.js
@@ -0,0 +1,5 @@
+const Engineer = require('./Engineer');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
